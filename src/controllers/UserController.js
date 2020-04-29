d36fcc4a36d63e1ee211a33b2be517b5683d26c8
maykbrito <maykbrito@gmail.com>
2020-04-28 21:37:13 -0300

    finalizando CRUD de usuários

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,7 +3,48 @@ const knex = require('../database')
 module.exports = {
     async index(req, res) { 
         const results = await knex('users')
-
         return res.json(results)
+    },
+    async create(req, res, next) {
+        try {
+            const { username } = req.body
+
+            await knex('users').insert({
+                username
+            })
+
+            return res.status(201).send()
+        } catch (error) {
+            next(error)
+        }
+    },
+    async update(req, res, next) {
+        try {
+            const { username } = req.body
+            const { id } = req.params
+            
+            
+            await knex('users')
+            .update({ username })
+            .where({ id })
+
+            return res.send()
+
+        } catch (error) {
+            next(error)
+        }
+    },
+    async delete(req, res, next) {
+        try {
+            const { id } = req.params
+
+            await knex('users')
+            .where({ id })
+            .del()
+
+            return res.send()
+        } catch (error) {
+            next(error)
+        }
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,10 @@ const routes = express.Router()
 const UserController = require('./controllers/UserController')
 
 routes.get('/users', UserController.index)
+routes.post('/users', UserController.create)
+routes.put('/users/:id', UserController.update)
+routes.delete('/users/:id', UserController.delete)
+
 
 
 module.exports = routes
